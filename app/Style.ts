0772b40aae6a634dca0bbc9b0c5c6ef72a22432c
Rahul Kardile <rahulkardile321@gmail.com>
2026-02-07 14:03:10 +0530

    react router setup

---
+++ app/Style.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native";
+
+const styles  = StyleSheet.create({
+    container : {
+        flex: 1,
+        backgroundColor: "#25292e",
+        alignItems: "center",
+        alignContent: "center"
+    },
+    text: {
+        color: "#fff"
+    },
+    button: {
+        color: "#fff",
+        textDecorationLine: "underline",
+        fontSize: 20
+    }
+});
+
+
+export default styles;
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,12 @@
 import { Text, View } from "react-native";
+import styles from "./Style";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text style={styles.text}>Hello World </Text>
+      <Link href={"/about"} style={styles.button}>About Page </Link>
     </View>
   );
 }
